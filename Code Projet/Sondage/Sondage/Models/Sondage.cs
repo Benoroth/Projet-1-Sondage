0401960d127ca9ffe77e79ae52a513487e4d608b
Timothee GRAF <adai05@SYSINF.POLE-FORMATION.FR>
2018-01-10 10:50:29 +0100

    Ajout du code C# .Net (début)

---
+++ Code Projet/Sondage/Sondage/Models/Sondage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sondage
+{
+    //sondage public ou privé
+    public enum TypeSondage
+    {
+        Public,
+        Private
+    }
+    //choix unique ou multiple
+    public enum TypeChoix
+    {
+        Unique,
+        Multiple
+    }
+    public class Sondage
+    {
+        private int nbVote { get; set; }
+        private string nomQuest { get; set; }
+        public string lienResult { get; set; }
+        public string lienPartage { get; set; }
+        private string lienSuppr { get; set; }
+        private bool PublicOuPrive { get; set; }
+        public int noteSond { get; set; }
+        private DateTime dateSond { get; set; }
+
+    }
+}

898a48c2d912fa10b89e8ca970715b2aea223e7d
Timothee GRAF <adai05@SYSINF.POLE-FORMATION.FR>
2018-01-16 09:51:09 +0100

    Ajout classes projet et début req SQL

---
+++ Code Projet/Sondage/Sondage/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace Sondage.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Adresse BDD SQL
+        private const string SqlConnectionString = @"Server=.\SQLExpress;Initial Catalog=Projet; Trusted_Connection=Yes";
+
+        ////Requètes SQL
+
+        //Récupérer nombre de votants en BDD
+
+
+
+    }
+}

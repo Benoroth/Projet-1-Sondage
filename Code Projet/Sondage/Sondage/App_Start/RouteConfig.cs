047a8cdf0b6ba4625d86263aa485644762a6febe
Emilien REISS <adai08@SYSINF.POLE-FORMATION.FR>
2018-01-16 09:42:58 +0100

    changement dans les vues

--- Code Projet/Sondage/Sondage/App_Start/RouteConfig.cs
+++ Code Projet/Sondage/Sondage/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace Sondage
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );           
         }
     }
 }

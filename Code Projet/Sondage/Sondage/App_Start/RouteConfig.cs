bdeee24a5ab5f13248f23efa6cb80d9d6105cf10
Emilien REISS <adai08@SYSINF.POLE-FORMATION.FR>
2018-02-05 11:03:55 +0100

    routage vers page d'accueil

--- Code Projet/Sondage/Sondage/App_Start/RouteConfig.cs
+++ Code Projet/Sondage/Sondage/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace Sondage
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Sondage", action = "Home" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            );            
         }
     }
 }
